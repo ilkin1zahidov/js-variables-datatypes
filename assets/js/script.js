0d6fb2a0b988b0badffd66290bcef3980a4b9621
ilkin1zahidov <ilkiniz@code.edu.az>
2022-08-18 03:18:27 +0400

    js,vatiables,datatypes

---
+++ assets/js/script.js
@@ -0,0 +1,210 @@
+//  alert("Hello p130");
+// console.log("hello world")
+
+// var name="Ilkin";
+
+// var surname="Zahidov"
+
+// console.log(name + " "+ surname);
+
+// var name = "Ilkin";
+// var name = "Seid";
+
+// let name1= "Ilkin";
+
+// let name2 ="Seid";
+
+// console.log(name1 + " " + name2);\
+
+// const age = 25;
+// console.log(age);
+
+// let age=50;
+// let surname = "Zahidov";
+// let isMarried="true"
+// let adress = null;
+// let count;
+// console.log(adress);
+// console.log(count);
+
+// let person ={
+//     name:"Ilkin",
+//     surname:"Zahidov",
+//     age:25
+// }
+
+//console.log(person.name + " "+ person.surname)
+//console.log(person);
+
+// let students = ["Ilkin", "Kenan",25]
+
+// for (let i= 0; i < students.length; i++) {
+ 
+//     console.log(students[i])
+// }
+
+// let person1 ={
+//     name:"Ilkin",
+//     surname:"Zahidov",
+//     age:25,
+
+//     phoneNumbers:[7483487, 48374]
+// }
+
+// let person2 = {
+//     name:"Seid",
+//     surname:"Zahidov",
+//     age:17,
+//     phoneNumbers:[7483487, 48374]
+// }
+
+// let students = [person1, person2];
+
+// for (let i= 0; i< students.length; i++) {
+   
+//     console.log(students[i]);
+// }
+
+// for (let i= 0; i< students.length; i++) {
+    
+// for (let j= 0; j< students[i].phoneNumbers. length; j++) {
+    
+//     console.log(students[i].phoneNumbers[j]);
+        
+//     }
+    
+// }
+
+
+// for (let i= 0; i < person1.phoneNumbers.length; i++) {
+//  console.log(person1.phoneNumbers[i])
+    
+// }
+
+// let numbers = [0,1,2,3,4,5,6,7];
+
+// numbers.forEach(item => {
+//     console.log(item);
+// })
+
+// let person1 ={
+//         name:"Ilkin",
+//         surname:"Zahidov",
+//         age:25,
+    
+//         phoneNumbers:[7483487, 48374]
+//     }
+    
+//     let person2 = {
+//         name:"Seid",
+//         surname:"Zahidov",
+//         age:17,
+//         phoneNumbers:[7483487, 48374]
+//     }
+
+
+
+//     let students =[{
+//         name:"Ilkin",
+//         surname:"Zahidov",
+//         age:25,
+//         phoneNumbers:[7483487, 48374]
+        
+//     },
+//     {
+//         name:"Seid",
+//         surname:"Zahidov",
+//         age:25,
+//         phoneNumbers:[7483487, 48374]
+
+//     },
+//     {
+//         name:"Orxan",
+//         surname:"Ekberov",
+//         age:26,
+//         phoneNumbers:[7483487, 48374] 
+
+//     }    
+// ];
+    
+    
+
+//     for (var i= 0; i< students.length; i++) {
+//         if(students[i].name=="Ilkin"){
+//             console.log(students[i].age+ " -"+ students[i].surname)
+//         }
+//         break;
+//     }
+  
+    // let num1 = 101;
+    // let num2 = 11;
+
+    // let result = num1%num2;
+
+    // console.log(result);
+
+    // for (let i= 0; i<=10; i++) {
+    //     if(i==0){
+    //         console.log("Ne tek ne de cutdur")
+    //     }else{
+    //         if(i%2==1){
+    //             console.log("Tek:",i)
+    //         }
+    //         else{
+    //             console.log("Cut:",i)
+    //         }
+    //     }
+       
+        
+    // }
+
+    //let isActive = true;
+
+    // if(!isActive){
+    //     console.log("Aktiv deil");
+    // }else{
+    //     console.log("Aktivdir");
+    // }
+
+    // let result= isActive ? "Active" : "Active deil";
+
+    // console.log(result);
+
+    // let studentSurname = null;
+
+    // let result = studentSurname ?? "student not found"
+
+    // console.log(result)
+
+
+    // let a = 101;
+    // let b = a;
+    // b = 10;
+
+    // console.log(a);
+    // console.log(b);
+
+    // let obj1={
+    //             name:"Ilkin",
+    //             surname:"Zahidov",
+    //             age:25,
+            
+    //             phoneNumbers:[7483487, 48374]
+    //         }
+
+    //         let obj2 = obj1;
+    //         obj2.age = 11;
+    //         console.log(obj1)
+
+    // let studenName;
+
+    // if (studentName == undefined){
+    //     console.log("Teyin olunmayib");
+    // }else{
+    //     console.log("teyin olunub");
+    // }
+
+
+    let isDeleted =null;
+
+    console.log(typeof isDeleted);
